ee99e902393582265a1228c6fad94110f60dc101
Taonahub <52515393+suthawadee@users.noreply.github.com>
2020-09-01 11:53:59 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char chr;
+    printf("Enter a character: ");
+    scanf("%c",&chr);
+    printf("You entered %c,"chr);
+
+    printf("ASCII value is %d.",chr);
+    return 0;
+}
